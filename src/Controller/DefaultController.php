6f2beb69e908c773f64ca3883eb7db1c6eb80316
mathurin petit <mathurin.petit@gmail.com>
2021-06-15 12:44:08 +0200

    projet

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+
+  /**
+    * @Route("/")
+    */
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('default/index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
