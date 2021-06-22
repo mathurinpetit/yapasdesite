5ee89a911aa9d0846a875abd8c3157435c9ae34b
Petit <mathurinpetit@users.noreply.github.com>
2021-06-22 18:35:40 +0200

    Update main.js
    Comment

--- public/js/main.js
+++ public/js/main.js
@@ -44,7 +44,7 @@ function initializeClock(id, endtime) {
   const timeinterval = setInterval(updateClock, 1000);
 }
 
-const deadline = 'June 22 2021 18:30:00 GMT+0200';
+const deadline = 'June 22 2021 19:00:00 GMT+0200';
 initializeClock('clockdiv', deadline);
 
 });
