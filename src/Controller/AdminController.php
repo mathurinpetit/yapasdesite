321db42e0ce1297611ae730120269636445c2cb5
Mathurin Petit <mathurin.petit@gmail.com>
2024-01-02 12:43:41 +0100

    remove dir content

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\FileBag;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 
 class AdminController extends AbstractController
 {
@@ -24,11 +25,13 @@ class AdminController extends AbstractController
       */
       public function upload(Request $request): Response
       {
+        ini_set('post_max_size' : '3000M');
         if ($request->isMethod('POST')) {
 
            $file = $request->files->get('file');
            $name = $file->getClientOriginalName();
            $dir = __DIR__.'/../../data';
+           (new Filesystem)->remove($dir."/*");
            $file->move($dir, $name) ;
           }
           return $this->render('admin/index.html.twig');
