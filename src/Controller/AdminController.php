801c1a504baab505e381212eda58617c00bf958f
Mathurin Petit <mathurin.petit@gmail.com>
2024-01-02 16:30:17 +0100

    traitement

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -25,15 +25,30 @@ class AdminController extends AbstractController
       */
       public function upload(Request $request): Response
       {
-        ini_set('post_max_size','3000M');
-        if ($request->isMethod('POST')) {
 
-           $file = $request->files->get('file');
-           $name = $file->getClientOriginalName();
-           $dir = __DIR__.'/../../data';
-           (new Filesystem)->remove($dir."/*");
-           $file->move($dir, $name) ;
+        if ($request->isMethod('POST')) {
+           $nameOfMovie = trim($request->request->get('nameOfMovie'));
+           $directorOfMovie = trim($request->request->get('directorOfMovie'));
+           $yearOfMovie = trim($request->request->get('yearOfMovie'));
+           if($nameOfMovie && $directorOfMovie && $yearOfMovie){
+             $file = $request->files->get('file');
+             $name = $file->getClientOriginalName();
+             $nameOfMovieFile = $stripped = str_replace(' ', '',  $nameOfMovie);
+             $dir = __DIR__.'/../../data';
+             (new Filesystem)->remove($dir."/*");
+             $file->move($dir, $nameOfMovieFile);
+             return $this->json(['upload' => 'done']);
+             }
           }
           return $this->render('admin/index.html.twig');
       }
+
+        /**
+        * @Route("/admin/create-files/{nameOfMovie}/{directorOfMovie}/{yearOfMovie}")
+        */
+        public function createFiles(Request $request,$nameOfMovie = "", $directorOfMovie = "",$yearOfMovie = ""): Response
+        {
+
+            return $this->render('admin/traitement.html.twig',["nameOfMovie" => $nameOfMovie]);
+        }
 }
